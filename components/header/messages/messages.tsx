ced2c473485a6cda9a0d3866ba94642ef5b3d86e
sarinazeitooni <zeitoonisarina@gmail.com>
2021-07-12 17:31:36 +0430

    added header icons

--- components/header/messages/messages.tsx
+++ components/header/messages/messages.tsx
@@ -4,75 +4,83 @@ import MoneyOffIcon from '@material-ui/icons/MoneyOff';
 import CheckCircleOutlineIcon from '@material-ui/icons/CheckCircleOutline';
 import CategoryIcon from '@material-ui/icons/Category';
 import PaymentIcon from '@material-ui/icons/Payment';
+import DetailsIcon from '@material-ui/icons/Details';
+
 const messages = {
-    inputPlaceHolder : "     جستجو در دیجی‌کالا …",
-    signIn : "ورود به حساب کاربری",
-    logIn : 'ورود',
-    location : 'لطفا شهر و استان خود را انتخاب کنید',
-    navigation : [
+    inputPlaceHolder: "     جستجو در دیجی‌کالا …",
+    signIn: "ورود به حساب کاربری",
+    logIn: 'ورود',
+    location: 'لطفا شهر و استان خود را انتخاب کنید',
+    navigation: [
         {
-            icon : <MenuIcon/>,
-            title : 'دسته بندی کالاها',
-            categories : true ,
-            discount : false ,
-            digi : false
+            icon: <MenuIcon/>,
+            title: 'دسته بندی کالاها',
+            categories: true,
+            discount: false,
+            digi: false
 
         },
-        {   icon : <StorefrontIcon/>,
-            title : 'سوپرمارکت',
-            categories : false ,
-            discount : false ,
-            digi : false
+        {
+            icon: <StorefrontIcon/>,
+            title: 'سوپرمارکت',
+            categories: false,
+            discount: false,
+            digi: false
         },
-        {   icon : <MoneyOffIcon/>,
-            title : 'تخفیف ها و پیشنهادها',
-            categories : false ,
-            discount : true ,
-            digi : false
+        {
+            icon: <MoneyOffIcon/>,
+            title: 'تخفیف ها و پیشنهادها',
+            categories: false,
+            discount: true,
+            digi: false
         },
-        {   icon : <CheckCircleOutlineIcon/>,
-            title : 'دیجی کالای من',
-            categories : false ,
-            discount : true ,
-            digi : false
+        {
+            icon: <CheckCircleOutlineIcon/>,
+            title: 'دیجی کالای من',
+            categories: false,
+            discount: true,
+            digi: false
         },
-        {   icon : <CategoryIcon/>,
-            title : 'دیجی پلاس',
-            categories : false ,
-            discount : false ,
-            digi : true,
-            digiComponent : {
-                url : 'https://www.digikala.com/static/files/38492329.jpg',
-                menu : false,
-                titleImage : 'https://www.digikala.com/static/files/4a2895fc.svg',
-                width : '648px'
+        {
+            icon: <CategoryIcon/>,
+            title: 'دیجی پلاس',
+            categories: false,
+            discount: false,
+            digi: true,
+            digiComponent: {
+                url: 'https://www.digikala.com/static/files/38492329.jpg',
+                menu: false,
+                titleImage: 'https://www.digikala.com/static/files/4a2895fc.svg',
+                width: '648px'
             }
         },
-        {   icon : '',
-            title : 'دیجی کلاب',
-            categories : false ,
-            discount : false ,
-            digi : true,
-            digiComponent : {
-                url : 'https://dkstatics-public.digikala.com/digiclub/53aa31f81138fd588df842086e152102979a00d7_1601458094.jpg',
-                titleImage : 'https://www.digikala.com/static/files/1c93eb76.svg',
-                width : '718px',
-                menu : true,
-                menuItem : [
+        {
+            icon: <DetailsIcon/>,
+            title: 'دیجی کلاب',
+            categories: false,
+            discount: false,
+            digi: true,
+            digiComponent: {
+                url: 'https://dkstatics-public.digikala.com/digiclub/53aa31f81138fd588df842086e152102979a00d7_1601458094.jpg',
+                titleImage: 'https://www.digikala.com/static/files/1c93eb76.svg',
+                width: '718px',
+                menu: true,
+                menuItem: [
                     'صفحه اصلی دیجی کلاب'
                 ]
             }
         },
-        {   icon : PaymentIcon,
-            title : 'دیجی پی',
-            categories : false ,
-            discount : false ,
-            digi : true,
-            digiComponent : {
-                url : 'https://www.digikala.com/static/files/148f56c6.png',
-                menu : true,
-                width : '586px',
-                menuItem : [
+        {
+            icon: <PaymentIcon/>,
+            title: 'دیجی پی',
+            categories: false,
+            discount: false,
+            digi: true,
+            digiComponent: {
+                url: 'https://www.digikala.com/static/files/148f56c6.png',
+                menu: true,
+                width: '586px',
+                menuItem: [
                     'فعالسازی کیف پول',
                     'صفحه اصلی دیجی پی',
                     'خرید شارژ',
@@ -82,16 +90,16 @@ const messages = {
             }
         },
         {
-            title : 'سوالی دارید؟',
-            categories : false ,
-            discount : false ,
-            digi : false
+            title: 'سوالی دارید؟',
+            categories: false,
+            discount: false,
+            digi: false
         },
         {
-            title : 'فروشنده  شوید',
-            categories : false ,
-            discount : false ,
-            digi : false
+            title: 'فروشنده  شوید',
+            categories: false,
+            discount: false,
+            digi: false
         }
     ]
 };
--- components/header/navigation/Navigation.tsx
+++ components/header/navigation/Navigation.tsx
@@ -1,10 +1,7 @@
-/* eslint-disable */
 import React,{Fragment} from 'react';
 import style from './scss/navigation.module.scss';
 import EditLocationOutlinedIcon from '@material-ui/icons/EditLocationOutlined';
 import Digi from "./dropdown/digi/Digi";
-
-// https://www.pluralsight.com/guides/how-to-show-and-hide-reactjs-components
 
 interface IProps {
     data: { location: string, navigation: { title: string, digi: boolean, icon : any , digiComponent: { url: string } }[] }
@@ -21,11 +18,12 @@ const Navigation: React.FC<IProps> = ({data}) => {
                     return (
                         <Fragment key={item.title}>
                             <div className={style['navbar-item']}>{item.title}
-                                {/*<span className={style['navbar-icon']}>{item.icon}</span>*/}
                                 <div className={style['drop-down']}>
                                     {item.digi && <Digi data={item.digiComponent}/>}
-                                    {/*{item.icon && }*/}
                                 </div>
+                                <span className={style['navbar-icon']}>{item.icon}
+                                <span className={style['club-icon']}/>
+                                </span>
                             </div>
                         </Fragment>
                     )
