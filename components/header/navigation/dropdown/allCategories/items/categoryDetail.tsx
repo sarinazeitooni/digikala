20a4c970641d3039162486149e464cfdfe14c046
sarinazeitooni <zeitoonisarina@gmail.com>
2021-07-13 13:37:00 +0430

    fixed drop down menu bug

--- components/header/navigation/dropdown/allCategories/items/categoryDetail.tsx
+++ components/header/navigation/dropdown/allCategories/items/categoryDetail.tsx
@@ -11,23 +11,27 @@ const CategoryDetail: React.FC<IProps> = ({title ,data}) => {
     return (
         <div className={style['category-detail-container']}>
             <h2 className={style['category-title']}> {messages.categoryText} {title} <ArrowBackIosIcon/></h2>
-            {data.map((index) => {
-                return (
-                    <div key={index.itemTitle} className={style['group-detail-item']}>
-                        <h4 className={style['detail-title']}>{index.itemTitle} <ArrowBackIosIcon/></h4>
-                        {index.itemOption &&
-                        <div className={style['detail-items']}>
-                            {index.itemOption.map((item)=>{
-                                return(
+            <div className={style['category-detail-container-flex']}>
+
+                {data.map((index) => {
+                    return (
+                        <div key={index.itemTitle} className={style['group-detail-item']}>
+                            <h4 className={style['detail-title']}>{index.itemTitle} <ArrowBackIosIcon/></h4>
+                            {index.itemOption &&
+                            <div className={style['detail-items']}>
+                                {index.itemOption.map((item)=>{
+                                    return(
                                         <div key={item} className={style['detail-item']}>{item}</div>
-                                )
-                            })}
+                                    )
+                                })}
+                            </div>
+                            }
                         </div>
-                        }
-                    </div>
-                )
-            })}
+                    )
+                })}
+            </div>
         </div>
+
     )
 }
 export default CategoryDetail;
